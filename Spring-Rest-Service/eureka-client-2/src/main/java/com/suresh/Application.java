426537ad53a21bf08b62d54ea6a2de55e25f2fb5
Suresh Arumugam <suresh12391@gmail.com>
2021-06-05 19:42:42 +0530

    Added 3 sample spring boot service with eureka registry

---
+++ Spring-Rest-Service/eureka-client-2/src/main/java/com/suresh/Application.java
@@ -0,0 +1,42 @@
+package com.suresh;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Application {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/")
+	public String home() throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("message", "Hello World... From Server 2");
+		jsonObject.put("message-3", new JSONObject(
+				restTemplate.exchange(
+						"http://localhost:8003/", HttpMethod.GET, null, String.class).getBody()));
+		return jsonObject.toString();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
